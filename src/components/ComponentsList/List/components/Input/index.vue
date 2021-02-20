62ca78e3e939fa3f436adf11032185eaa1ecd60a
sky_bear <wangpeng@tzdtz.com>
2021-02-20 18:05:30 +0800

    feat: 项目搭建

---
+++ src/components/ComponentsList/List/components/Input/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <input />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+class Input extends Vue {}
+export default Input;
+</script>
+
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
